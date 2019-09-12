460fd6566488e2595413376310b150f8501eb826
Kleber Brito Moreira <kleberbritomoreira10@gmail.com>
2019-09-12 10:04:05 -0300

    Creating an ERC-20 Token Standard
    Signed-off-by: Kleber Brito Moreira <kleberbritomoreira10@gmail.com>

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,12 +1,24 @@
 pragma solidity ^0.4.18;
 
 contract DappToken {
+    //Name
+    string public name = "DApp Token";
+    //Symbol
+    string public symbol = "DAPP";
+    //Standard
+    string public standard = "DApp Token v1.0";
+
+    uint256 public totalSupply;
+
+    mapping(address => uint256) public balanceOf;
+
     // Constructor
     // Set the total number of tokens
     // Read the total number of tokens
-    uint256 public totalSupply;
-
-    function DappToken() public {
-        totalSupply = 1000000;
+    function DappToken(uint256 _initialSupply) public {
+      balanceOf[msg.sender] = _initialSupply;
+      totalSupply = _initialSupply;
+      //allocate the initial supply
     }
+
 }
