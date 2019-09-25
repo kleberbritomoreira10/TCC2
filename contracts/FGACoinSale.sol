9c03181652c39efddbf4a141d977acb6c081d8fd
Kleber Brito Moreira <kleberbritomoreira10@gmail.com>
2019-09-25 19:20:31 -0300

    Creating contract to buy tokens
    Signed-off-by: Kleber Brito Moreira <kleberbritomoreira10@gmail.com>

--- contracts/FGACoinSale.sol
+++ contracts/FGACoinSale.sol
@@ -6,6 +6,9 @@ contract FGACoinSale {
   address admin;
   FGACoin public tokenContract;
   uint256 public tokenPrice;
+  uint256 public tokensSold;
+
+  event Sell(address _buyer, uint256 _amount);
 
   function FGACoinSale(FGACoin _tokenContract, uint256 _tokenPrice) public {
     // Assign an admin
@@ -14,6 +17,25 @@ contract FGACoinSale {
     tokenContract = _tokenContract;
     // Token Price
     tokenPrice = _tokenPrice;
+  }
+
+  //Multiply
+  function multiply(uint x, uint y) internal pure returns (uint z) {
+    require(y == 0 || (z = x * y) / y == x);
+  }
+
+  // Buying the tokens for the correct amount
+  function buyTokens(uint256 _numberOfTokens) public payable {
+    //Require that value is equal to tokens
+    require(msg.value == multiply(_numberOfTokens, tokenPrice));
+    //Require that the contract has enough tokens
+    require(tokenContract.balanceOf(this) >= _numberOfTokens);
+    //Require that a transfer is successful
+    require(tokenContract.transfer(msg.sender, _numberOfTokens));
+    tokensSold += _numberOfTokens;
+    //Trigger Sell Event
+    Sell(msg.sender, _numberOfTokens);
+    //Keep track of TokensSold
   }
 
 }
--- test/FGACoinSale.js
+++ test/FGACoinSale.js
@@ -1,11 +1,18 @@
 var FGACoinSale = artifacts.require('./FGACoinSale.sol');
+var FGACoin = artifacts.require('./FGACoin.sol');
 
 contract('FGACoinSale', function(accounts) {
   var tokenSaleInstance;
+  var tokenInstance;
+  var admin = accounts[0];
+  var buyer = accounts[1];
   var tokenPrice = 1000000000000000000; // em wei (menor subdivisão/unidade do ether)
+  var tokensAvailable = 750000;
+  var numberOfTokens;
 
   it('Inicializando o contrato com os valores corretos', function(){
       return FGACoinSale.deployed().then(function(instance){
+        //Manter a rastreabilidade da Instância
         tokenSaleInstance = instance;
         return tokenSaleInstance.address
       }).then(function(address){
@@ -18,5 +25,7 @@ contract('FGACoinSale', function(accounts) {
         assert.equal(price, tokenPrice, 'Preço do token está correto');
       });
   });
+
+  
 
 })
